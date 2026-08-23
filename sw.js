deea9d9978b52bba6d1e2c90b256557f3539bfd4
mAs-87 <asim.shahzad@uon.edu.pk>
2026-08-23 20:52:53 +0500

    Update cache version from v43 to v44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "uon-gpa-v43";
+const CACHE = "uon-gpa-v44";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
