18e39fa77ffe39ab6e576287c5b8c69516aabe28
mAs-87 <asim.shahzad@uon.edu.pk>
2026-08-23 21:42:27 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "uon-gpa-v45";
+const CACHE = "uon-gpa-v47";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
